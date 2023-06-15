375272c8540cc474fecbd4540939b9b6e7c95648
gazeifie <gazeifie012@gmail.com>
2023-06-16 01:57:05 +0900

    一部レイアウト修正・追加コンテンツ

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+window.addEventListener('load', ()=>{
+   const loading = document.getElementById('loading_page');
+   setTimeout(() => {
+      loading.classList.add('hide');
+   }, 1000)
+
+})
+
+
 document.addEventListener("DOMContentLoaded",()=>{
 });
 
